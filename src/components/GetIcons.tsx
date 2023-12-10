fc65019080c38d90b0ed9e398827ca4ec181e880
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-12-10 02:12:02 -0400

    dinamyc icons

---
+++ src/components/GetIcons.tsx
@@ -0,0 +1,22 @@
+import * as Io from "react-icons/io";
+
+interface Props {
+  icon: string;
+  className?: string;
+}
+
+const GetIcon = ({ icon, className }: Props) => {
+  const getIcon = (iconName: string) => {
+    const iconsMap = new Map();
+    iconsMap.set("Io", Io);
+
+    return iconsMap.get(iconName.substring(0, 2));
+  };
+
+  const icons = getIcon(icon);
+  const TheIcon = icons[icon];
+
+  return <TheIcon className={className} />;
+};
+
+export default GetIcon;
