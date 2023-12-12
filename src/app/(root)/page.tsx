81cf1e5245063038f9c89875a51b9c84ba42e747
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-12-12 11:40:29 -0400

    home page

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { SectionFeatures, SectionHero } from "./section";
+
+const Home = () => {
+  return (
+    <>
+      <SectionHero />
+      <SectionFeatures />
+    </>
+  );
+};
+
+export default Home;
