e43eb6fca36f9bfac76fe907a12a7e60c5738ccd
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-12-13 08:27:14 -0400

    update barrel

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Container } from "./Container";
 export { default as GetIcons } from "./GetIcons";
 export { default as GradientImg } from "./GradientImg";
+export { default as Navbar } from "./Navbar";
