e07b4402562d944ee65b36ac2a69abc5f48e5555
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-12-13 17:32:53 -0400

    layout root of aplication

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib";
+import { Navbar } from "@/components";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Chat Vyper",
+  description: "Services for your business",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className="dark">
+      <body
+        className={cn(
+          "min-w-screen font-sans antialiased grainsy dark:bg-gray-950",
+          inter.className,
+        )}
+      >
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
