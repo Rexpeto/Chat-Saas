57884c7912a395ac1838e10f0d669e7e77eb0ad9
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-12-17 00:22:33 -0400

    update

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { default as GradientImg } from "./GradientImg";
 export { default as Navbar } from "./Navbar";
 export { default as Theme } from "./Theme";
 export { default as SwitchTheme } from "./SwitchTheme";
+export { default as Providers } from "./Providers";
