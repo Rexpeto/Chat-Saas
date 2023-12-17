47065798954d7113dd2eec1c762c1dcf973755f8
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-12-17 00:29:34 -0400

    use providers of kinde

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib";
-import { Navbar, Theme } from "@/components";
+import { Navbar, Providers, Theme } from "@/components";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,18 +17,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={cn(
-          "min-w-screen font-sans antialiased grainsy dark:bg-gray-950",
-          inter.className,
-        )}
-      >
-        <Theme>
-          <Navbar />
-          {children}
-        </Theme>
-      </body>
+    <html lang="en">
+      <Providers>
+        <body
+          className={cn(
+            "min-w-screen font-sans antialiased grainsy dark:bg-gray-950",
+            inter.className,
+          )}
+        >
+          <Theme>
+            <Navbar />
+            {children}
+          </Theme>
+        </body>
+      </Providers>
     </html>
   );
 }
