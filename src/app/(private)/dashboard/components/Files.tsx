0c2a80f742eca3c65e476bb62766910d27b383ac
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-12-19 01:30:05 -0400

    use file card

--- src/app/(private)/dashboard/components/Files.tsx
+++ src/app/(private)/dashboard/components/Files.tsx
@@ -2,12 +2,22 @@
 
 import { trpc } from "@/app/_trpc/client";
 import { GetIcons } from "@/components";
+import FileCard from "./FileCard";
 
 const Files = () => {
   const { data: files, isLoading } = trpc.getUserFiles.useQuery();
 
   return files && files.length !== 0 ? (
-    <div></div>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8 divide-zinc-200 dark:divide-zinc-700">
+      {files
+        .sort(
+          (a, b) =>
+            new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime(),
+        )
+        .map((file) => (
+          <FileCard key={file.id} file={file} />
+        ))}
+    </div>
   ) : isLoading ? (
     <div className="w-full mt-24 flex justify-center">
       <GetIcons
