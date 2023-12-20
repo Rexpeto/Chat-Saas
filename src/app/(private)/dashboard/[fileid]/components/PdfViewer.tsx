a342a39b3e8d7cb76b12642bfac186e2c381dc09
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-12-20 18:05:19 -0400

    initial pdf viewer

---
+++ src/app/(private)/dashboard/[fileid]/components/PdfViewer.tsx
@@ -0,0 +1,11 @@
+const PdfViewer = () => {
+  return (
+    <div className="flex flex-col items-center bg-gray-200/60 dark:bg-gray-800 rounded shadow w-full">
+      <div className="flex items-center justify-between h-14 w-full px-2 border-b border-zinc-200 dark:border-zinc-700">
+        <div className="flex items-center gap-1.5">top bar</div>
+      </div>
+    </div>
+  );
+};
+
+export default PdfViewer;
