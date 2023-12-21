f184a7644ca28510c36e8a9e9405bef2e548f1e9
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-12-21 15:42:21 -0400

    update barrel

--- src/app/(private)/dashboard/components/index.ts
+++ src/app/(private)/dashboard/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Dashboard } from "./Dashboard";
 export { default as UploadButton } from "./UploadButton";
 export { default as Files } from "./Files";
+export { default as UploadDropzone } from "./UploadDropzone";
