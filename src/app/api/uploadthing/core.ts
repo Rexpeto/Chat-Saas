b98a5788a7c74c920fddcdef01c032ab5f70b055
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-12-23 15:38:56 -0400

    config pdfUploader with uploadthing

---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,23 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+
+const f = createUploadthing();
+
+const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
+
+// FileRouter for your app, can contain multiple FileRoutes
+export const ourFileRouter = {
+  // Define as many FileRoutes as you like, each with a unique routeSlug
+  pdfUploader: f({ image: { maxFileSize: "16MB" } })
+    .middleware(async ({ req }) => {
+      const { getUser } = getKindeServerSession();
+      const user = getUser();
+
+      if (!user || !user.id) throw new Error("Unauthorized");
+
+      return { userId: user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {}),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
