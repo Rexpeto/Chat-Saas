1b0aae08b5c1315a8c9177af465606019be3cb03
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-12-25 03:13:32 -0400

    webpack config disable canvas

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        canvas: require.resolve("canvas"),
+      };
+    }
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
