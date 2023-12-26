2444e057b6d21c07d483ad477b1d5ca79e647651
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-12-26 01:15:24 -0400

    tools of pdf in top bar

--- src/app/(private)/dashboard/[fileid]/components/PdfViewer.tsx
+++ src/app/(private)/dashboard/[fileid]/components/PdfViewer.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { GetIcons } from "@/components";
-import { useToast } from "@/components/ui";
+import { Button, Input, useToast } from "@/components/ui";
+import { useState } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
@@ -17,10 +18,49 @@ const PdfViewer = ({ url }: Props) => {
   const { toast } = useToast();
   const { width, ref } = useResizeDetector();
 
+  const [numPages, setNumPages] = useState<number>(1);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
   return (
-    <div className="flex flex-col items-center bg-gray-200/60 dark:bg-gray-800 rounded shadow w-full">
+    <div className="flex flex-col items-center bg-white dark:bg-gray-900 rounded shadow-lg w-full">
       <div className="flex items-center justify-between h-14 w-full px-2 border-b border-zinc-200 dark:border-zinc-700">
-        <div className="flex items-center gap-1.5">top bar</div>
+        <div className="flex items-center gap-1.5">
+          <Button
+            arial-label="Previous page"
+            className="p-2"
+            variant="ghost"
+            onClick={() =>
+              setCurrentPage((prev) => (prev - 1 > 1 ? prev - 1 : 1))
+            }
+          >
+            <GetIcons icon="MdArrowBackIos" className="m-0" />
+          </Button>
+
+          <div className="flex items-center gap-1.5">
+            <Input
+              className="w-16 h-8 text-sm"
+              min={1}
+              max={numPages}
+              type="number"
+              value={currentPage}
+              onChange={(e) => setCurrentPage(Number(e.target.value))}
+            />
+            <p className="space-x-1 text-sm text-zinc-700 dark:text-zinc-400">
+              <span>/</span> <span>{numPages}</span>
+            </p>
+          </div>
+
+          <Button
+            arial-label="Next page"
+            className="p-2"
+            variant="ghost"
+            onClick={() =>
+              setCurrentPage((prev) => (prev + 1 <= numPages ? prev + 1 : prev))
+            }
+          >
+            <GetIcons icon="MdArrowForwardIos" className="m-0" />
+          </Button>
+        </div>
       </div>
 
       <div className="flex-1 max-h-screen w-full">
@@ -41,10 +81,11 @@ const PdfViewer = ({ url }: Props) => {
                 variant: "destructive",
               })
             }
+            onLoadSuccess={({ numPages }) => setNumPages(numPages)}
             file={url}
             className="max-h-full"
           >
-            <Page width={width ? width : 1} pageNumber={1} />
+            <Page width={width ? width : 1} pageNumber={currentPage} />
           </Document>
         </div>
       </div>
